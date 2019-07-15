9da4855a3a7df64cd72865ef0ca2a74164ce3e61
Rafael Moreno <alejandro8924@gmail.com>
2019-07-15 01:28:27 -0500

    first commit

---
+++ src/Providers/PlansServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace RafaelMorenoJS\Plans\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class PlansServiceProvider
+ * @package RafaelMorenoJS\Plans\Providers
+ */
+class PlansServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
